e022cd15b46b18a24b975f9294199e189730abe8
Carlos Quijano <carlos@crqgestion.es>
2014-12-10 10:55:51 +0100

    getting started

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 (function () {
+	"use strict";
 	
-	var server = MashupPlatform.prefs.get('server');
+	var server = MashupPlatform.prefs.get('history_server');
 	var entity = 'Room1';
 	var entity_type = 'Room';//Room2_Room
 	var url= server+'/'+entity+'_'+entity_type;
 
+	  var layout,
+        ngsi,
+        form,
+        currentData,
+        error,
+        info;
+
+	
+	console.log('NGSI connection');
+	ngsi = new NGSI.Connection(MashupPlatform.prefs.get('ngsi_server'), {
+				use_user_fiware_token: true,
+	});
+	console.log(ngsi);
+	
 	MashupPlatform.http.makeRequest(url, {
 			method: 'GET',
 			onSuccess: function (response) {
+					console.log('HTTP connection');
+					console.log(response);
 					var forecast_data;
 					forecast_data = JSON.parse(response.responseText);
 					if (forecast_data.error) {
@@ -22,4 +39,7 @@
 			}
 	});
 
+	window.addEventListener("DOMContentLoaded", init, false);
+	
+	
 })();
